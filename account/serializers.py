3d2b907f7da4c21b75c032573a86fa6be5cf5845
tturgum5ekov <tturgum5ekov@gmail.com>
2023-06-21 21:01:51 +0600

    initial commit

---
+++ account/serializers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ('password',)
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=8, max_length=20, required=True, write_only=True)
+    password2 = serializers.CharField(min_length=8, max_length=20, required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'password2', 'first_name', 'last_name', 'avatar', 'username')
+
+    def validate(self, attrs):
+        password = attrs['password']
+        password2 = attrs.pop('password2')
+        if password != password2:
+            raise serializers.ValidationError('Passwords didn\'t math')
+        validate_password(password)
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
